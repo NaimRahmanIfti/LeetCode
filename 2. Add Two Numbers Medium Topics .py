80d98591b1a3d45a290a3a52179c95c283ebc576
NaimRahmanIfti <naimrahmanifti@gmail.com>
2024-11-03 19:08:16 -0800

    Leetcode Problem Solve

--- 2. Add Two Numbers Medium Topics .py
+++ 2. Add Two Numbers Medium Topics .py
@@ -5,13 +5,14 @@ class ListNode:
         self.next = next
 class Solution:
     def addTwoNumbers(self, l1, l2):
-
         list = ListNode()
         carry = 0
         while l1 or l2 is not None or carry != 0:
-            l1.val = l1.val if l1 else 0
-            l2.val = l2.val if l2 else 0
-            sum = l1.val + l2.val + carry
+            if l1:
+                l1.val = l1.val + carry
+            if l2:
+                l2.val = l2.val + carry
+            sum = l1.val + l2.val 
             carry = sum // 10
             list.next = ListNode(sum % 10)
             list = list.next
@@ -23,8 +24,8 @@ class Solution:
 
 
 
-l1 = ListNode([2,4,3])
-l2 = ListNode([5,6,4])
+l1 = [2,4,3]
+l2 = [5,6,4]
 s = Solution()
 print(s.addTwoNumbers(l1, l2))
 
