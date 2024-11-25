b1347c6a8112738a8308017035a68da451731da6
NaimRahmanIfti <naimrahmanifti@gmail.com>
2024-11-25 12:54:17 -0800

    Merge remote-tracking branch 'origin/main'

--- 28. Find the Index of the First Occurrence in a String.py
+++ 28. Find the Index of the First Occurrence in a String.py
@@ -5,11 +5,23 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        try:
-            i = haystack.index(needle)
-            return i
-        except:
-            return -1
-haystack = "hello world"
-needle = "world"
+        # try:
+        #     i = haystack.index(needle)
+        #     return i
+        # except:
+        #     return -1
+
+        for i in range(len(haystack)):
+            j = 0
+            for k in range(i, len(haystack)):
+                if haystack[k] == needle[j]:
+                    j += 1
+                else:
+                    break
+                if j == len(needle):
+                    return i
+
+        return -1
+haystack = "hello"
+needle = "ll"
 print(Solution().strStr(haystack, needle))
